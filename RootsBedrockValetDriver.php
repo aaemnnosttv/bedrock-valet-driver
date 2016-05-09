b74ea35b5fec0994b6269353c7a2c623bdd4af0e
Evan Mattson <me@aaemnnost.tv>
2016-05-09 11:52:05 -0400

    initial commit

---
+++ RootsBedrockValetDriver.php
@@ -0,0 +1,57 @@
+<?php
+
+class BedrockValetDriver extends BasicValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return void
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return (
+            file_exists("$sitePath/config/application.php")
+            && file_exists("$sitePath/web/wp-config.php")
+            && is_dir("$sitePath/web/app/")
+        );
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        $staticFilePath = $sitePath.'/web/'.$uri;
+
+        if (file_exists($staticFilePath) && ! is_dir($staticFilePath)) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        if (0 === strpos($uri, '/wp/')) {
+            return $sitePath.$uri;
+        }
+
+        return $sitePath.'/web/index.php';
+    }
+}
